30163a21432763ae30513e251b33deb9cc138b29
leo1997v <gravel1997@outlook.com>
2021-05-29 21:51:36 +0800

    implement request layer and logic layer of function 'create monitor'

--- src/back-end/predictive_monitor/main.py
+++ src/back-end/predictive_monitor/main.py
@@ -4,6 +4,7 @@
 
 from fastapi import APIRouter, UploadFile, File
 from typing import Optional, List
+from predictive_monitor.task_handler import store_and_send, get_tasks_by_id
 
 predictive_monitor = APIRouter()
 
@@ -11,17 +12,18 @@ predictive_monitor = APIRouter()
 # put interfaces here
 
 @predictive_monitor.get('/')
-async def default():
+def default():
     return {'message': 'This is predictive monitor module'}
 
 
 @predictive_monitor.post('/create-monitor')
-async def create_monitor(monitor_name: str, eventlog: UploadFile = File(...)):
-    task_uuid = sort_and_store({{'monitor_name': monitor_name, "eventlog": eventlog}})
-    return task_uuid
+# def create_monitor(monitor_name: str, eventlog: UploadFile = File(...)):
+def create_monitor(monitor_name: str, eventlog: str):
+    task_uuid = store_and_send({'monitor_name': monitor_name, "eventlog": eventlog})
+    return {"UUID": task_uuid}
 
 
 @predictive_monitor.get('/poll/{task_id}')
-async def poll(task_id: str):
+def poll(task_id: str):
     tasks = task_id.split('&')
     return get_tasks_by_id(tasks)
