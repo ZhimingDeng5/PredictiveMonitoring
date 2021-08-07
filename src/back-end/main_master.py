adad51326337706964a85ef36f93124997924cf4
Zejjs <a.s.poniatowski@gmail.com>
2021-08-07 23:15:17 +1000

    Task cancellation implemented in worker.

--- src/back-end/main_master.py
+++ src/back-end/main_master.py
@@ -6,7 +6,7 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from api.request_handler import request_handler
-from services.queue_controller import ThreadedConsumer
+from services.queue_controller import ThreadedMasterConsumer
 from services.task_manager import TaskManager
 
 app = FastAPI(
@@ -28,7 +28,7 @@ app.include_router(request_handler)
 # start separate thread for listening to output
 @app.on_event("startup")
 def startup():
-    td = ThreadedConsumer(TaskManager())
+    td = ThreadedMasterConsumer(TaskManager())
     td.start()
 
 
--- src/back-end/main_worker.py
+++ src/back-end/main_worker.py
@@ -1,4 +1,34 @@
-from services.queue_controller import subscribeToInputQueue
+from uuid import UUID
+
+from services.cancel_request import CancelRequest
+from services.cancellation_handler import CancellationHandler
+from services.queue_controller import subscribeToCancelQueue, ThreadedWorkerConsumer
+
+cancellations: CancellationHandler = CancellationHandler()
+
+
+def cancel_callback(ch, method, properties, body):
+    req = CancelRequest.fromJsonS(body.decode())
+    taskID: UUID = req.taskID
+    was_cancelled: bool = req.cancelled
+
+    # another worker has cancelled this task, remove it from cancel set
+    if was_cancelled and cancellations.hasCancel(taskID):
+        cancellations.removeCancel(taskID)
+        print(f"Another node cancelled task with ID {taskID}. Removed it from the cancel set.")
+
+    # the node is currently working on this task. Stop and delete its files
+    elif taskID == cancellations.getCurrentTask():
+        td.stop()
+
+    # the task is in the queue or at another worker node. Add its id to cancel set
+    else:
+        cancellations.addCancel(taskID)
+        print(f"Received request to cancel task with ID {taskID}. Added its ID to cancel set.")
+
 
 if __name__ == '__main__':
-    subscribeToInputQueue()
+    td = ThreadedWorkerConsumer(cancellations)
+    td.start()
+
+    subscribeToCancelQueue(cancel_callback)
