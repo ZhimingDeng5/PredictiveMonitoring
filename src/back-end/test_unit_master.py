6e2528ab6b51b73dabc2f3ef3c36e5f196e9a33b
Samuel Tumewa <stumewa@yahoo.com>
2021-08-29 18:43:28 +1000

    Removed nirdizati testing for now

--- src/back-end/test_unit_master.py
+++ src/back-end/test_unit_master.py
@@ -7,21 +7,22 @@ from main_master import app
 
 client = TestClient(app)
 
-def test_start_with_no_tasks():
-    response = client.get("/tasks")
-    assert response.status_code == 200
-    assert response.json() == {'tasks': []}
-
+# Testing the GET /tasks/{id} endpoint before any processing
+# Expected: 404
 def test_task_not_found_error():
     taskID = str(uuid4())
     response = client.get(f"/tasks/{taskID}")
     assert response.status_code == 404
 
+# Testing the GET /dashboard/{id} endpoint before any processing
+# Expected: 404
 def test_dashboard_not_found_error():
     taskID = str(uuid4())
     response = client.get(f"/dashboard/{taskID}")
     assert response.status_code == 404
 
+# Testing the CANCEL /task/{id} endpoint before any processing
+# Expected: 404
 def test_dashboard_cancellation_not_found_error():
     taskID = str(uuid4())
     response = client.delete(f"/cancel/{taskID}")
