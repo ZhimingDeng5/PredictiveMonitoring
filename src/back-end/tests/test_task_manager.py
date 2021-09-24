622d538cb36559d1c66df0fd9114b5899008a5c2
Samuel Tumewa <stumewa@yahoo.com>
2021-09-24 13:12:42 +1000

    Added mocking to task manager tests

--- src/back-end/tests/test_task_manager.py
+++ src/back-end/tests/test_task_manager.py
@@ -7,71 +7,59 @@ from services.task import Task
 from services.task_manager import TaskManager
 
 
-# Task format:
-# ts = {"00000000-0000-0000-0000-000000000000": {"py/object": "services.task.Task", "taskID": "00000000-0000-0000-0000-000000000000", "predictors_path": "task_files\\00000000-0000-0000-0000-000000000000-predictors", "schema_path": "task_files\\00000000-0000-0000-0000-000000000000-schema", "event_log_path": "task_files\\00000000-0000-0000-0000-000000000000-event_log", "status": "COMPLETED"}}
-t_id = "00000000-0000-0000-0000-000000000000"
-t_uuid = UUID(t_id)
-
+@pytest.fixture
+def empty_tm():
+    empty_tm = TaskManager()
+    return empty_tm
 
 @pytest.fixture
-def tm():
-    tm = TaskManager()
-    return tm
+def task_uuid():
+    task_uuid = uuid4()
+    return task_uuid
 
 @pytest.fixture
-def ts():
-    ts = {t_id: Task(t_id, "", "", "", Task.Status.QUEUED)}
-    return ts
+def task_status(task_uuid):
+    task_id = str(task_uuid)
+    task_status = jsonpickle.encode({task_id: Task(task_id, "", "", "", Task.Status.QUEUED)})
+    return task_status
 
-@pytest.fixture(autouse = True)
-def maintain_task_status(ts):
-    # Save current task_status before each task
-    with open(os.path.join("..", "persistence", "task_status"), "r") as f_in:
-        old_ts = jsonpickle.decode(f_in.read())
-    with open(os.path.join("..", "persistence", "task_status"), "w") as f_out_test:
-        f_out_test.write(jsonpickle.encode(ts))
-
-    # Perform test
-    yield
-    
-    # Rewrite old task_status after each task
-    with open(os.path.join("..", "persistence", "task_status"), "w") as f_out:
-        f_out.write(jsonpickle.encode(old_ts))
-
+@pytest.fixture
+def tm(empty_tm, task_status, mocker):
+    m = mocker.patch('builtins.open', mocker.mock_open(read_data = task_status))
+    empty_tm.getStateFromDisk()
+    return empty_tm
 
 # Tests the proper initialisation of the CancellationHandler
-def test_init(tm):
-    assert tm.getAllTasks() == {"tasks": []}
+def test_init(empty_tm):
+    assert empty_tm.getAllTasks() == {"tasks": []}
 
-# Tests updating own task_status from disk
-def test_update_from_disk(tm):
-    tm.getStateFromDisk()
-    assert tm.hasTask(t_uuid)
+# Tests updating own task status from disk
+def test_update_from_disk(empty_tm, task_uuid, task_status, mocker):
+    m = mocker.patch('builtins.open', mocker.mock_open(read_data = task_status))
+    empty_tm.getStateFromDisk()
+    assert empty_tm.hasTask(task_uuid)
+
+# Tests updating own task status from network
+def test_update_from_network(empty_tm, task_uuid, task_status, mocker):
+    m = mocker.patch('services.task_manager.requestFromQueue', return_value = task_status)
+    empty_tm.getStateFromNetwork()
+    assert empty_tm.hasTask(task_uuid)
 
 # Tests the removal of tasks
-def test_remove_task(tm):
-    tm.getStateFromDisk()
-    tm.removeTask(t_uuid)
-    assert not tm.hasTask(t_uuid)
+def test_remove_task(tm, task_uuid):
+    tm.removeTask(task_uuid)
+    assert not tm.hasTask(task_uuid)
     assert tm.getAllTasks() == {"tasks": []}
 
 # Tests the update of tasks
-def test_update_task(tm):
-    tm.getStateFromDisk()
-    t1 = tm.getTask(t_uuid)
-    t2 = Task(t_id, "", "", "", Task.Status.PROCESSING)
+def test_update_task(tm, task_uuid):
+    t1 = tm.getTask(task_uuid)
+    t2 = Task(task_uuid, "", "", "", Task.Status.PROCESSING)
     tm.updateTask(t2)
-    t3 = tm.getTask(t_uuid)
+    t3 = tm.getTask(task_uuid)
     assert t1 != t3
 
 # Tests the cancellation of tasks
-def test_cancel_task(tm):
-    tm.getStateFromDisk()
-    tm.cancelTask(t_uuid)
-    assert tm.getTask(t_uuid).status == Task.Status.CANCELLED.name
-
-# Tests the export of the task status
-def test_pickle_cancel(tm, ts):
-    tm.getStateFromDisk()
-    ts_pickled = tm.getAllTasksPickled()
-    assert ts == jsonpickle.decode(ts_pickled)
+def test_cancel_task(tm, task_uuid):
+    tm.cancelTask(task_uuid)
+    assert tm.getTask(task_uuid).status == Task.Status.CANCELLED.name
