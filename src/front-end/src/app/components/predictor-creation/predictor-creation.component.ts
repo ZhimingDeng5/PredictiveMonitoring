d49ea9b440bc3cf65459a216e948617378d41e35
ZhimingDeng5 <zhimingd@student.unimelb.edu.au>
2021-09-30 18:36:39 +0800

    predictor-trainig: config files upload

--- src/front-end/src/app/components/predictor-creation/predictor-creation.component.ts
+++ src/front-end/src/app/components/predictor-creation/predictor-creation.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import axios from "axios";
+import {environment} from "../../../environments/environment";
+import {LocalStorageService} from "../../local-storage.service";
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-predictor-creation',
   templateUrl: './predictor-creation.component.html',
@@ -10,17 +13,18 @@ export class PredictorCreationComponent implements OnInit {
 
   isAdvanced: boolean = false;
   userForm : FormGroup;
-
+  eventLog:File;
+  schema:File;
   toggleMode() {
     this.isAdvanced = !this.isAdvanced;
   }
 
-  constructor(private fb:FormBuilder){
+  constructor(private fb:FormBuilder,public LocalStorage: LocalStorageService,private router:Router){
 
     this.userForm = this.fb.group({
       predictorName :['',Validators.required],
       eventlog :['',Validators.required],
-
+      schema:['',Validators.required],
       predictorType:['label',Validators.required],
       bucketingType:['zero',Validators.required],
       encodingType:['agg',Validators.required],
@@ -43,19 +47,26 @@ export class PredictorCreationComponent implements OnInit {
       dt_max_depth :['5',Validators.required]
     })
   }
-
+  EventLogUpload(event) {
+    this.eventLog= <File>event.target.files[0];
+    console.log(this.eventLog);
+  }
+  SchemaUpload(event) {
+    this.schema= <File>event.target.files[0];
+    console.log(this.schema);
+  }
   onSubmit() : void{
-    let string1 = 
+    let string1 =
       '{"'+this.userForm.value.predictorType+'":'+
       '{"'+this.userForm.value.bucketingType+'":'+
       '{"'+this.userForm.value.encodingType+'":'+
       '{"'+this.userForm.value.learnerType+'":';
-   
+
     let paramstring ="";
     if (this.userForm.value.learnerType=="xgb")
     {
-      paramstring = 
-      '{"n_estimators": ' + this.userForm.value.xgb_n_estimators + 
+      paramstring =
+      '{"n_estimators": ' + this.userForm.value.xgb_n_estimators +
       ',"learning_rate": ' + this.userForm.value.xgb_learning_rate +
       ',"colsample_bytree": ' + this.userForm.value.xgb_colsample_bytree +
       ',"subsample": ' + this.userForm.value.xgb_subsample +
@@ -63,36 +74,63 @@ export class PredictorCreationComponent implements OnInit {
     }
     else if (this.userForm.value.learnerType=="rf")
     {
-      paramstring = 
-      '{"n_estimators": ' + this.userForm.value.rf_n_estimators + 
+      paramstring =
+      '{"n_estimators": ' + this.userForm.value.rf_n_estimators +
       ',"max_features": ' + this.userForm.value.rf_max_features;
     }
     else if (this.userForm.value.learnerType=="gbm")
     {
-      paramstring = 
-      '{"n_estimators": ' + this.userForm.value.gbm_n_estimators + 
+      paramstring =
+      '{"n_estimators": ' + this.userForm.value.gbm_n_estimators +
       ',"max_features": ' + this.userForm.value.gbm_max_features +
       ',"learning_rate": ' + this.userForm.value.gbm_learning_rate;
     }
     else if (this.userForm.value.learnerType=="dt")
     {
-      paramstring = 
-      '{"max_features": ' + this.userForm.value.dt_max_features + 
+      paramstring =
+      '{"max_features": ' + this.userForm.value.dt_max_features +
       ',"max_depth": ' + this.userForm.value.dt_max_depth;
     }
-    
     let full_string = string1+paramstring+'}}}}}';
-    console.log(full_string);
+    //console.log(full_string);
     let serializedName = JSON.parse(full_string);
-    let serializedForm1 = JSON.stringify(serializedName);
-       
-    var element = document.createElement('a');
+    let serializedForm1:string = JSON.stringify(serializedName);
+    let predictorName:String=this.userForm.value.predictorName;
+    //console.log(serializedForm1);
+    let blob = new Blob([serializedForm1], { type: 'text/plain' });
+    let configfile:File = new File([blob], "config.json", {type: "text/plain"});
+    console.log(configfile);
+    let formData = new FormData();
+    formData.append("event_log", this.eventLog);
+    formData.append("schema", this.schema);
+    formData.append("config", configfile);
+        axios.post(environment.backend + "/create-predictor", formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }).then((res) => {
+          if(res.status==200) {
+            localStorage[res.data.task_id + "Name"] = this.userForm.value.predictorName;
+            console.log("Config files uploaded successfully!")
+          }
+
+            // window.location.href= './dashboard';
+            //this.router.navigateByUrl('/dashboard');
+
+            //this.selectedMonitor.taskid=res.data;
+            //window.location.href='/monitor-viewing';
+            //console.log(this.selectedMonitor.taskid);
+
+
+
+    })
+/*    var element = document.createElement('a');
     element.setAttribute('href', "data:text/json;charset=UTF-8," + encodeURIComponent(serializedForm1));
     element.setAttribute('download', "config.json");
     element.style.display = 'none';
     document.body.appendChild(element);
     element.click(); // simulate click
-    document.body.removeChild(element);
+    document.body.removeChild(element);*/
   }
 
   ngOnInit(): void {
