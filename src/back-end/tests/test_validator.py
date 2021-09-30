a50089ebc4c3f5826e8535d0be8d8525217c24e2
jiahaoxu-se <jiahaox3@student.unimelb.edu.au>
2021-10-01 01:28:27 +0800

    [fix&clean] datetime validation

--- src/back-end/tests/test_validator.py
+++ src/back-end/tests/test_validator.py
@@ -3,8 +3,6 @@ import pytest
 import services.validator as va
 import services.file_handler as fh
 import pandas as pd
-# import json
-# import copy
 
 
 @pytest.fixture
@@ -63,41 +61,3 @@ def test_type_check():
 def test_check_timestamp():
     timestamp = "2016/10/10 18:27:48"
     assert va.check_timestamp(timestamp)
-
-
-# def validator_test():
-#     # Correct pickle file
-#     path_p = "../../../DataSamples/bpi/predictors/test-label-predictor.pkl"
-#     json_p = fh.pickleLoadingAsDict(path_p)
-#
-#     # Wrong pickle file
-#     json_p_w = copy.deepcopy(json_p)
-#     json_p_w[0] = 1
-#
-#     # Corrext CSV file
-#     path_c = "../../../DataSamples/bpi/test-event-log.csv"
-#     cf = pd.read_csv(path_c, index_col=False)
-#     json_c = cf.to_json(orient='records')
-#     path_json = "../../../DataSamples/bpi/test-single-event.json"
-#
-#     # Wrong CSV file
-#     json_c_w = copy.deepcopy(json_c)
-#     params_json = json.loads(json_c_w)
-#     params_json[0]["Case ID"] = "test"
-#     json_c_w = json.dumps(params_json)
-#
-#     # Schema file
-#     path_s = "../../../DataSamples/bpi/test-schema.json"
-#     json_s = open(path_s).read()
-#
-#     # Perform test
-#     test_validate_pickle(json_p, json_p_w)
-#     test_validate_pickle_in_file(path_p)
-#     test_type_check()
-#     test_validate_csv_in_path(path_c, path_s)
-#     test_validate_json_in_path(path_json, path_s)
-#     test_validate_by_schema(json_c, json_c_w, json_s)
-#     test_check_timestamp()
-#
-#
-# validator_test()
