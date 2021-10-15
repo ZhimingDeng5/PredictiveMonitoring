6333b01d4598a6f550d38bccee01ae633d726cfe
jiahaoxu-se <jiahaox3@student.unimelb.edu.au>
2021-10-15 15:36:14 +0800

    clean the code

--- src/prediction/services/validator.py
+++ src/prediction/services/validator.py
@@ -60,11 +60,20 @@ def type_check(obj, ts):
 # used for validating the event log (csv) file by schema
 def validate_csv_in_path(csv_path: str, schema_path: str):
     try:
-        cf = pd.read_csv(csv_path, index_col=False, low_memory=False)
+        with open(csv_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            first_line = lines[0]
+        de = ","
+        if first_line.count(";") > first_line.count(","):
+            de = ";"
+        cf = pd.read_csv(csv_path, index_col=False, low_memory=False, delimiter=de)
         types = cf.dtypes
         schema = open(schema_path).read()
         params_json = json.loads(schema)
         items = params_json.items()
+    except Exception as e:
+        return response(False, "validator error: " + str(e))
+    try:
         for key, value in items:
             # schema item with a list of cols
             if str(value).find('[') != -1:
@@ -75,9 +84,6 @@ def validate_csv_in_path(csv_path: str, schema_path: str):
                     elif str(key).lower().find('timestamp') != -1:
                         if types[name] is None or not check_timestamp(cf[name]):
                             return response(False, '\"' + str(name) + '\" should be a timestamp')
-                    # elif str(key).lower().find('future_values') != -1:
-                    #     if types[name] is None or types[name] != "bool":
-                    #         return response(False, '\"' + str(name) + '\" should be a bool')
                     else:
                         if types[name] is None or \
                                 (types[name] != "object" and types[name] != "int64" and
@@ -92,9 +98,6 @@ def validate_csv_in_path(csv_path: str, schema_path: str):
                 elif str(key).lower().find('timestamp') != -1:
                     if types[name] is None or not check_timestamp(cf[name]):
                         return response(False, '\"' + str(name) + '\" should be a timestamp')
-                # elif str(key).lower().find('future_values') != -1:
-                #     if types[name] is None or types[name] != "bool":
-                #         return response(False, '\"' + str(name) + '\" should be a bool')
                 else:
                     if types[name] is None or \
                             (types[name] != "object" and types[name] != "int64" and
@@ -103,76 +106,6 @@ def validate_csv_in_path(csv_path: str, schema_path: str):
         return response(True, "correct")
     except Exception as e:
         return response(False, "Missing col: "+str(e))
-
-
-# # used for validating the event log (json) file by schema
-# def validate_json_in_path(json_path: str, schema_path: str):
-#     try:
-#         jf = pd.read_json(json_path)
-#         s = jf.to_json(orient='records')
-#         schema = open(schema_path).read()
-#         return validate_by_schema(s, schema)
-#     except Exception as e:
-#         return response(False, "Wrong type for json file or schema file: "+str(e))
-
-
-# # Get the single event log and check each of them
-# def validate_by_schema(event_str: str, schema: str):
-#     event_list = json.loads(event_str)
-#     params_json = json.loads(schema)
-#     items = params_json.items()
-#     # record the number of line which the system is checking
-#     line_count = 1
-#     for event_json in event_list:
-#         for key, value in items:
-#             # schema item with a list of cols
-#             if str(value).find('[') != -1:
-#                 for name in list(value):
-#                     res = validate(name, event_json, key, line_count)
-#                     if not res['isSuccess']:
-#                         return res
-#             # schema item with a single col
-#             else:
-#                 res = validate(value, event_json, key, line_count)
-#                 if not res['isSuccess']:
-#                     return res
-#         line_count += 1
-#     message = 'structure correct :)'
-#     return response(True, message)
-
-
-# # main validate function
-# # @name: col name
-# # @event_json: single event log json
-# # @key: schema information containing the type of col
-# def validate(name, event_json, key, index):
-#     # check if the event log has the attribute
-#     if str(name) not in event_json:
-#         message = '[' + str(index) + '] without the column for \"' + name + '\"'
-#         return response(False, message)
-#
-#     # check the digital cols
-#     if str(key).lower().find('num') != -1 or str(key).lower().find('id') != -1 or str(key).lower().find('ignore') != -1:
-#         if event_json[name] is not None and event_json[name] != "" and \
-#                 str(type(event_json[name])).split('\'')[1] not in ['int', 'float', 'double']:
-#             message = '\"' + str(name) + '\" should be a number'
-#             return response(False, message)
-#
-#     # check the timestamp cols
-#     if str(key).lower().find('timestamp') != -1:
-#         if event_json[name] is not None:
-#             res = check_timestamp(event_json[name])
-#             if not res["isSuccess"]:
-#                 message = '[' + str(index) + '] \"' + str(name) + '\": ' + res["msg"]
-#                 return response(False, message)
-#
-#     # check the bool cols
-#     if str(key).lower().find('future_values') != -1:
-#         if event_json[name] is not None and \
-#                 'bool' not in str(type(event_json[name])):
-#             message = '\"' + str(name) + '\" should be a boolean'
-#             return response(False, message)
-#     return response(True, '[' + str(index) + '] line correct')
 
 
 # check the timestamp
@@ -206,15 +139,6 @@ def validate_config(config_path: str, schema_path: str):
                 target_key not in schema_json["static_cat_cols"] and \
                 target_key not in schema_json["static_num_cols"]:
             return response(False, target_key + " is not a parameter of config json")
-        # ui_data_key = list(config_json.keys())[1]
-        # if ui_data_key != "ui_data":
-        #     return response(False, target_key + " is not a parameter of config json")
-        # for n, n_v in config_json[ui_data_key].items():
-        #     if n not in ["log_file", "job_owner", "start_time"]:
-        #         return response(False, n + " is not a parameter of ui data")
-        # evaluation_key = list(config_json.keys())[2]
-        # if len(config_json) == 3 and evaluation_key != "evaluation":
-        #     return response(False, evaluation_key + " is not a parameter of config json")
         bucket = config_json[target_key]
         if len(bucket) != 1:
             return response(False, target_key + " should only have one attribute")
@@ -280,8 +204,3 @@ def validate_config(config_path: str, schema_path: str):
         return response(True, "config file is correct")
     except Exception as e:
         return response(False, str(e))
-
-
-print(validate_csv_in_path("../../../DataSamples/bpi/test-event-log-large.csv",
-                           "../../../DataSamples/bpi/test-schema.json"))
-# print(validate_config("../../../DataSamples/bpi/myconfig_remtime.json", "../../../DataSamples/bpi/test-schema.json"))
