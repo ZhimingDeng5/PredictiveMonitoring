64ce073a043e58348d722756662acc6bd0823678
Fanyu Meng <fanyu.meng@student.unimelb.edu.au>
2021-10-19 20:34:36 +0800

    include updated file_handler

--- src/commons/file_handler.py
+++ src/commons/file_handler.py
@@ -1,13 +1,9 @@
 import os
 import json
-
 from typing import List
-
 from fastapi.datastructures import UploadFile
 import pandas as pd
 import pickle
-import base64
-
 import shutil
 import zipfile
 
@@ -15,8 +11,6 @@ import zipfile
 predict_root = 'predict_files'
 training_root = 'training_files'
 predictor = 'predictor'
-
-
 
 #--------------------------convertion functions-------------------------------------
 # csv -> parquet
@@ -27,7 +21,6 @@ def csv2Parquet(input_path:str , output_path:str):
   cf = pd.read_csv(input_path, index_col = False)
   cf.to_parquet(output_path)
 
-
 # parquet -> csv
 def parquet2Csv(input_path:str, output_path:str):
   if not fileExistanceCheck([input_path]):
@@ -35,7 +28,6 @@ def parquet2Csv(input_path:str, output_path:str):
     return 'Parquet file not found'
   pf = pd.read_parquet(input_path)
   pf.to_csv(output_path, index = False)
-
 
 # csv -> json format
 def csv2Json(input_path:str, output_path:str):
@@ -45,7 +37,6 @@ def csv2Json(input_path:str, output_path:str):
   cf = pd.read_csv(input_path)
   cf.to_json(output_path, orient='records')
 
-
 # json file -> csv
 def json2Csv(input_path:str, output_path:str):
   if not fileExistanceCheck([input_path]):
@@ -54,8 +45,6 @@ def json2Csv(input_path:str, output_path:str):
   jf = pd.read_json(input_path)
   jf.to_csv(output_path, index=False)
   print(f'File has been saved at {output_path}')
-
-
 
 def parquetGenerateCsv(input_path:str):
   if not fileExistanceCheck([input_path]):
@@ -76,13 +65,11 @@ def csvLoadingAsString(input_path):
   s = cf.to_json()
   return s
 
-
 # loading CSV file into json dict
 def csvLoadingAsDict(input_path:str):
   s = csvLoadingAsString(input_path)
   jd = json.loads(s)
   return jd
-
 
 # loading json file into string(jsonFormat)(used inside)
 def jsonLoadingAsString(js_path:str):
@@ -90,24 +77,20 @@ def jsonLoadingAsString(js_path:str):
   s = jf.to_json()
   return s
 
-
 # loading json file into dict
 def jsonLoadingAsDict(js_path:str):
   s = jsonLoadingAsString(js_path)
   jd = json.loads(s)
   return jd
 
-
 # loading pickle file into dict
 def pickleLoadingAsDict(pickle_path:str):
   if not os.path.exists(pickle_path):
     print('pickle not exist')
     return 'pickle not exist'
-
   with open(pickle_path,'rb') as pf:
     pd = pickle.load(pf)
   return pd
-
 
 # load root address
 def loadPredictRoot(uuid:str,  additional_address = ''):
@@ -116,9 +99,8 @@ def loadPredictRoot(uuid:str,  additional_address = ''):
   if not os.path.exists(address):
     print(f'Task {uuid} not exist')
     return 'Root not exist'
-
+    
   return address
-
 
 def loadTrainingRoot(uuid:str,  additional_address = ''):
   address = os.path.join(additional_address,training_root,uuid)
@@ -136,9 +118,7 @@ def savePredictEventlog(uuid: str, file: UploadFile, additional_address = ''):
 
   root_address = os.path.join(additional_address,predict_root,uuid)
 
-  
   return saveFile(root_address, file)
-
 
 # load EventLog address
 def loadPredictEventLogAddress(uuid: str, file_name: str, additional_address = ''):
@@ -147,7 +127,6 @@ def loadPredictEventLogAddress(uuid: str, file_name: str, additional_address = '
     return 'Eventlog not found'
 
   return root_address
-
 
 # save training Eventlog
 def saveTrainingEventlog(uuid: str, file:UploadFile, additional_address = ''):
@@ -162,6 +141,8 @@ def saveTrainingEventlog(uuid: str, file:UploadFile, additional_address = ''):
 #load training Eventlog address
 def loadTrainingEventLogAddress(uuid:str, file_name:str, additional_address = ''):
   root_address = root_address = os.path.join(additional_address,training_root,uuid,file_name)
+  if not os.path.exists(root_address):
+    return 'Eventlog not found'
 
   return root_address
 
@@ -181,7 +162,6 @@ def savePredictor(uuid: str, files:List[UploadFile], additional_address = ''):
   
   return 'Pickles saved'
 
-
 # load pickle file address by uuid and name
 def loadPredictorAddress(uuid: str, additional_address = ''):
   root_address = root_address = os.path.join(additional_address,predict_root,uuid,predictor)
@@ -194,8 +174,9 @@ def loadPredictorAddress(uuid: str, additional_address = ''):
 #--------------------------------Schema functions-------------------------------------------------
 # save Schema dict as pickle file
 def savePredictSchema(uuid: str, file: UploadFile, additional_address = ''):
-  if not schemaCheck(file.filename):
+  if not jsonCheck(file.filename):
     return 'Schema file not accept'
+
   root_address = os.path.join(additional_address,predict_root,uuid)
   
   return saveFile(root_address, file)
@@ -208,15 +189,13 @@ def loadPredictSchemaAddress(uuid: str, file_name: str, additional_address = '')
 
   return root_address
 
-
 def saveTrainingSchema(uuid: str, file: UploadFile, additional_address = ''):
-  if not schemaCheck(file.filename):
+  if not jsonCheck(file.filename):
     return 'Schema file not accept'
 
   root_address = os.path.join(additional_address,training_root,uuid)
 
   return saveFile(root_address, file)
-
 
 def loadTrainingSchemaAddress(uuid: str, file_name: str, additional_address = ''):
   root_address = root_address = os.path.join(additional_address,training_root,uuid,file_name)
@@ -226,18 +205,18 @@ def loadTrainingSchemaAddress(uuid: str, file_name: str, additional_address = ''
   return root_address
 
 # -----------------------------Config functions---------------------------------------------------
-def saveConfig(uuid: str, file: UploadFile, volume_address=''):
-  if not schemaCheck(file.filename):
+def saveConfig(uuid: str, file: UploadFile, additional_address=''):
+  if not jsonCheck(file.filename):
     return 'Config file not accept'
 
-  root_address = os.path.join(volume_address, training_root, uuid)
+  root_address = os.path.join(additional_address, training_root, uuid)
 
   return saveFile(root_address, file)
 
 
 # load EventLog address
-def loadConfigAddress(uuid: str, file_name: str, volume_address=''):
-    root_address = os.path.join(volume_address, training_root, uuid, file_name)
+def loadConfigAddress(uuid: str, file_name: str, additional_address=''):
+    root_address = os.path.join(additional_address, training_root, uuid, file_name)
     if not os.path.exists(root_address):
       return 'Config not found'
 
@@ -258,12 +237,12 @@ def loadPredictResult(uuid: str,additional_address=''):
   return 'Result not found'
 
 
-def loadTrainingResult(uuid:str, additional_address=''):
+def loadTraingingResult(uuid:str, additional_address=''):
   root_address = os.path.join(additional_address,training_root,uuid)
   allFile = os.listdir(root_address)
 
   for file in allFile:
-    if file == uuid + '-results.zip':
+    if file == uuid + '-results.pkl':
       return os.path.join(root_address,file)
 
 
@@ -278,7 +257,6 @@ def fileExistanceCheck(files: List):
   
   return result
 
-
 # check the file in csv format
 def csvCheck(file: str):
 
@@ -289,7 +267,6 @@ def csvCheck(file: str):
   else:
     return False
 
-
 # check the  file in pickle format
 def pickleCheck(file: str):
   filename,extension = os.path.splitext(file)
@@ -299,16 +276,14 @@ def pickleCheck(file: str):
   else:
     return False
 
-
 # check schema file in json format
-def schemaCheck(file: str):
+def jsonCheck(file: str):
   filename,extension = os.path.splitext(file)
   
   if extension == '.json':
     return True
   else:
     return False
-
 
 # check parquet file
 def parquetCheck(file: str):
@@ -319,49 +294,34 @@ def parquetCheck(file: str):
   else:
     return False
 
-
-# check config file in json format
-def configCheck(file: str):
-    filename, extension = os.path.splitext(file)
-  
-    if extension == '.json':
-        return True
-    else:
-        return False
-
-  
-
 # ------------------------------zip functions-----------------------------------------------------
-def zipFile(uuid: str, keep_files: bool = True, volume_address: str = ''):
-    startdir = os.path.join(volume_address, training_root, uuid)
+def zipFile(uuid: str, keep_files: bool = True, additional_address: str = ''):
+  startdir = os.path.join(additional_address, training_root, uuid)
 
-    if not os.path.exists(startdir):
-        return False
+  if not os.path.exists(startdir):
+      return False
 
-    # z = zipfile.ZipFile(os.path.join(startdir, f"{uuid}-results.zip"), 'w', zipfile.ZIP_DEFLATED)
-  
-    zip_contents = []
-    for dirpath, dirnames, filenames in os.walk(startdir):
-        for filename in filenames:
-            zip_contents.append((os.path.join(dirpath, filename), filename))
-            # z.write(os.path.join(dirpath, filename))
-    # z.close()
+  zip_contents = []
+  for dirpath, dirnames, filenames in os.walk(startdir):
+      for filename in filenames:
+          zip_contents.append((os.path.join(dirpath, filename), filename))
 
-    with zipfile.ZipFile(os.path.join(startdir, f"{uuid}-results.zip"), "w", zipfile.ZIP_DEFLATED) as zipped:
-        for abs_filename, filename in zip_contents:
-            zipped.write(abs_filename, arcname=filename)
-            if not keep_files:
-                os.remove(abs_filename)
-  
-    # shutil.rmtree(startdir)
-    return True
-
+  with zipfile.ZipFile(os.path.join(startdir, f"{uuid}-results.zip"), "w", zipfile.ZIP_DEFLATED) as zipped:
+      for abs_filename, filename in zip_contents:
+          zipped.write(abs_filename, arcname=filename)
+          if not keep_files:
+              os.remove(abs_filename)
+              
+  return True
 
 # load zip address by uuid
-def loadZip(uuid: str, volume_address=''):
-    zip_address = os.path.join(volume_address, training_root, uuid)+'.zip'
+def loadZip(uuid: str, additional_address=''):
+  zip_address = os.path.join(additional_address, training_root, uuid, uuid + '-results.zip')
 
-    return zip_address
+  if not os.path.exists(zip_address):
+    return 'zip result not found'
+
+  return zip_address
 
 #--------------------------------Delete functions-------------------------------------------------
 def removePredictTaskFile(uuid: str, additional_address = ''):
@@ -372,7 +332,6 @@ def removePredictTaskFile(uuid: str, additional_address = ''):
   else:
     return f'Task {uuid} not found'
 
-
 def removeTrainingTaskFile(uuid: str, additional_address=''):
   rm_pass = os.path.join(additional_address, training_root, uuid)
   if os.path.exists(rm_pass):
@@ -380,7 +339,6 @@ def removeTrainingTaskFile(uuid: str, additional_address=''):
     return f'Task {uuid} has been deleted'
   else:
     return f'Task {uuid} not found'
-
 
 def removeFile(path:str):
   if os.path.exists(path):
@@ -391,7 +349,6 @@ def removeFile(path:str):
     
 #------------------------------common functions---------------------------------------------------
 def saveFile(address:str, file:UploadFile):
-
   if not os.path.exists(address):
     os.makedirs(address)
 
